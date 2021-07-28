295add645f9d28aaf58563dde062ebcbc970c43e
DOJ <omarjeng999@gmail.com>
2021-07-28 16:50:17 +0000

    add desert section and change some syles

--- src/components/meals/Meals.jsx
+++ src/components/meals/Meals.jsx
@@ -2,16 +2,23 @@ import React from 'react';
 import {Fragment} from "react";
 import SideNav from './SideNAv'
 import Section from './Section'
+import './Meals.css'
+import Desert from './Desert'
 
 const Meals = props => {
   return (
     <Fragment>
-      <div className="row">
-        <div className="col-md-4">
-          <SideNav/>
-        </div>
-        <div className="col-md-8">
-          <Section/>
+      <div className="meals py-3 px-3">
+        <div className="row">
+          <div className="col-md-3">
+            <SideNav/>
+          </div>
+          <div className="col-md-8 offset-md-1">
+            <Section/>
+          </div>
+          <div className="col-md-12">
+            <Desert/>
+          </div>
         </div>
       </div>
     </Fragment>
